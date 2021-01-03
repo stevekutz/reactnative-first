c8f4e8e4819249ec28dfaa970f4c4e9a3ca1c01b
stevekutz <stkutz@gmail.com>
2021-01-03 16:09:00 -0600

    added Pressable and logic to display levels of press state interaction

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar} from 'expo-status-bar';
 import React, {useState, useEffect}  from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Alert, Pressable } from 'react-native';
 
 export default function App() {
 
-	// create basic state controlled via TouchableHighlight
+	// create basic state controlled via TouchableHighlight and Pressable
 	const [outputText, setOutputText] = useState(" Open App.js to start experimenting ! ")
 	const [orig, setToggleOrig] = useState(false)
+	const [timesPressed, setTimesPressed] = useState(0);
+
+	// renders dynamic message text within Pressable
+	let textLog = '';
+	if (timesPressed > 1) {
+		textLog = timesPressed + 'x onPress';
+	} else if (timesPressed > 0) {
+		textLog = 'onPress';
+	}
+
 
 	// useEffect runs when outputText updated and will toggle on first render as well
 	useEffect( () => {
@@ -27,6 +37,43 @@ export default function App() {
 			>
 				<Text> Toggle Me </Text>		
 			</TouchableHighlight>
+			<Button
+				title = "Button"
+				color="#841584"
+				onPress = {() => Alert.alert('Button Pressed')}	
+			/>
+
+			<Pressable
+				onPress={() => {
+					setTimesPressed((current) => current + 1);
+				}}
+				onLongPress = {() => Alert.alert('Long Press detected')}
+				style={({ pressed }) => [
+				{
+					backgroundColor: pressed
+					? 'rgb(210, 230, 255)'
+					: 'white'
+				},
+				styles.wrapperCustom
+				]}>
+				{({ pressed }) => (
+				<View>
+					<Button
+						style = {{color: 'green'}}
+						title = {pressed ? 'Button Pressed! title' : 'Press Me Button title'}			
+					/>
+
+					<Text style={styles.text}>
+						{pressed ? 'Pressed!' : 'Press Me'}
+					</Text>
+					<Text>
+						{textLog}
+					</Text>
+				</View>
+
+
+				)}
+			</Pressable>
 
 			<StatusBar style="auto" />
 		</View>
@@ -53,7 +100,14 @@ const styles = StyleSheet.create({
 		padding: 2,
 		margin: 15,
 		backgroundColor: 'lightyellow'
-	}
+	},
+	wrapperCustom: {
+		borderRadius: 8,
+		padding: 6
+  	},
+    text: {
+		fontSize: 16
+  	},
 });
 
 // <Button title = "Change Text" onPress = {() => setOutputText("The text changed !")}/>
